2754b615c98d9c0181a98ccc484ef2d7b88348a2
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2024-10-13 20:24:15 +0800

    Init Spring Boot

---
+++ SUSSYCourses/src/main/java/com/sustech/cs309/project/sussycourses/sussycourses/SussyCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.sustech.cs309.project.sussycourses.sussycourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SussyCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SussyCoursesApplication.class, args);
+    }
+
+}
---
+++ SUSSYCourses/src/test/java/com/sustech/cs309/project/sussycourses/sussycourses/SussyCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustech.cs309.project.sussycourses.sussycourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SussyCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
