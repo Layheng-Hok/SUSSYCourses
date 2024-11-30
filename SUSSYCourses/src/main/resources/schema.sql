a1b8db090dde807f326354cfd11f94479fdcae64
haoyanghy <aarontan113388@gmail.com>
2024-11-30 15:30:47 +0800

    courseware view and download

--- SUSSYCourses/src/main/resources/schema.sql
+++ SUSSYCourses/src/main/resources/schema.sql
@@ -178,14 +178,18 @@ VALUES ('Java - Beginner to Advanced', 'blank', 4, 'open', 'approved', 'Programm
         NOW());
 
 
-INSERT INTO courseware (course_id, file_type, category, url, downloadable, chapter, courseware_order, variant_of,
-                        version, display_version, created_at)
-VALUES (1, 'mp4', 'lecture', 'chapter1', FALSE, 1, 1, 1, 1, TRUE, NOW()),
-       (1, 'pdf', 'lecture', 'intro_python.pdf', FALSE, 1, 2, 2, 1, TRUE, NOW()),
-       (1, 'mp4', 'lecture', 'advanced_js.mp4', FALSE, 1, 3, 3, 1, TRUE, NOW()),
-       (2, 'md', 'assignment', 'datascience_overview.md', FALSE, 1, 1, 4, 1, TRUE, NOW()),
-       (1, 'mp4', 'lecture', 'chapter1_version2', FALSE, 1, 1, 1, 2, FALSE, NOW()),
-       (1, 'pdf', 'assignment', 'Unsupervised Learning.pdf', FALSE, 1, 1, 6, 1, TRUE, NOW());
+INSERT INTO courseware (course_id, file_type, category, url, downloadable, chapter, courseware_order,variant_of, version, display_version, created_at) VALUES
+(1,'mp4', 'lecture', 'chapter1', FALSE, 1,1, 1, 1, TRUE, NOW()),
+(1,'pdf', 'lecture', 'intro_python.pdf', FALSE, 1,2, 2, 1, TRUE,NOW()),
+(1,'mp4', 'lecture', 'advanced_js.mp4', FALSE, 1,3, 3, 1, TRUE,NOW()),
+(2,'md', 'assignment', 'datascience_overview.md', FALSE, 1,1, 4, 1, TRUE, NOW()),
+(1, 'mp4', 'lecture', 'chapter1_version2', FALSE, 1, 1, 1, 2, FALSE,NOW()),
+(1, 'pdf', 'assignment', 'Unsupervised learning.pdf', FALSE, 1, 1, 1, 1, TRUE,NOW()),
+(1, 'mp4', 'lecture', 'lecture2.mp4', FALSE, 2, 1, 1, 1, TRUE,NOW()),
+(1, 'pptx', 'project', 'Overview.pptx', FALSE, 1, 1, 1, 1, TRUE,NOW()),
+(1, 'md', 'project', 'README.md', FALSE, 2, 1, 1, 1, TRUE,NOW())
+
+;;
 
 INSERT INTO course_student (course_id, student_id, status, liked)
 VALUES (1, 3, 'enrolled', TRUE),
